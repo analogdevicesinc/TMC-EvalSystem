a52a473feef424e39673cf3393d8c4e7fba13755
Leonard Kugis <leonard@kug.is>
2019-02-27 17:18:16 +0100

    .

--- hal/Timer.h
+++ hal/Timer.h
@@ -1,16 +1,16 @@
 #ifndef TIMER_H_
-	#define TIMER_H_
+#define TIMER_H_
 
-	#include "derivative.h"
+#include "derivative.h"
 
-	typedef struct
-	{
-		void (*init) (void);
-		void (*deInit) (void);
-		void (*setDuty) (uint16 duty);
-		uint16 (*getDuty) (void);
-	} TimerTypeDef;
+typedef struct
+{
+	void (*init) (void);
+	void (*deInit) (void);
+	void (*setDuty) (uint16 duty);
+	uint16 (*getDuty) (void);
+} TimerTypeDef;
 
-	TimerTypeDef Timer;
+TimerTypeDef Timer;
 
 #endif /* TIMER_H_ */
