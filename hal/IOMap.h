ee6ca1a890abe7276a571626967baf81f018945c
Leonard Kugis <leonard@kug.is>
2023-01-19 21:49:32 +0100

    IOMap: Reordered Pins
    Now they are grouped for individual platforms.
    Indices for neccessary pins for e.g. ChipClick remain unchanged.

--- hal/IOMap.h
+++ hal/IOMap.h
@@ -9,30 +9,22 @@ typedef struct
 
 	IOPinTypeDef **pins; // Map Pin ID <=> Pin
 
-#if defined(LandungsbrueckeV3)
-	IOPinTypeDef VM_MEAS;
-	IOPinTypeDef AIN0;
-	IOPinTypeDef AIN1;
-	IOPinTypeDef AIN2;
-#endif
-
 	IOPinTypeDef ID_CLK;
-//	IOPinTypeDef ID_CH0;
-//	IOPinTypeDef ID_CH1;
-//
-//	IOPinTypeDef DIO0;
-//	IOPinTypeDef DIO1;
-//	IOPinTypeDef DIO2;
-//	IOPinTypeDef DIO3;
-//	IOPinTypeDef DIO4;
-//	IOPinTypeDef DIO5;
-//
-//	IOPinTypeDef DIO6;
-//	IOPinTypeDef DIO7;
-//	IOPinTypeDef DIO8;
-//	IOPinTypeDef DIO9;
-//	IOPinTypeDef DIO10;
-//	IOPinTypeDef DIO11;
+	IOPinTypeDef ID_CH0;
+	IOPinTypeDef ID_CH1;
+
+	IOPinTypeDef DIO0;
+	IOPinTypeDef DIO1;
+	IOPinTypeDef DIO2;
+	IOPinTypeDef DIO3;
+	IOPinTypeDef DIO4;
+	IOPinTypeDef DIO5;
+	IOPinTypeDef DIO6;
+	IOPinTypeDef DIO7;
+	IOPinTypeDef DIO8;
+	IOPinTypeDef DIO9;
+	IOPinTypeDef DIO10;
+	IOPinTypeDef DIO11;
 	IOPinTypeDef CLK16;
 	IOPinTypeDef SPI2_CSN0;
 	IOPinTypeDef SPI2_CSN1;
@@ -45,60 +37,57 @@ typedef struct
 	IOPinTypeDef SPI1_SCK;
 	IOPinTypeDef SPI1_SDI;
 	IOPinTypeDef SPI1_SDO;
-//
-//	IOPinTypeDef DIO12;
-//	IOPinTypeDef DIO13;
-//	IOPinTypeDef DIO14;
-//	IOPinTypeDef DIO15;
-//	IOPinTypeDef DIO16;
-//	IOPinTypeDef DIO17;
-//	IOPinTypeDef DIO18;
-//	IOPinTypeDef DIO19;
-//
-	IOPinTypeDef WIRELESS_TX;
-	IOPinTypeDef WIRELESS_RX;
-	IOPinTypeDef WIRELESS_NRST;
-//
+
+	IOPinTypeDef DIO12;
+	IOPinTypeDef DIO13;
+	IOPinTypeDef DIO14;
+	IOPinTypeDef DIO15;
+	IOPinTypeDef DIO16;
+	IOPinTypeDef DIO17;
+	IOPinTypeDef DIO18;
+	IOPinTypeDef DIO19;
+
 	IOPinTypeDef RS232_TX;
 	IOPinTypeDef RS232_RX;
-//
-//	IOPinTypeDef USB_V_BUS;
-//	IOPinTypeDef USB_V_DM;
-//	IOPinTypeDef USB_V_DP;
+
+	IOPinTypeDef USB_V_BUS;
+	IOPinTypeDef USB_V_DM;
+	IOPinTypeDef USB_V_DP;
 
 	IOPinTypeDef LED_STAT;
 	IOPinTypeDef LED_ERROR;
 
-//	IOPinTypeDef EXTIO_2;
-//	IOPinTypeDef EXTIO_3;
-//	IOPinTypeDef EXTIO_4;
-//	IOPinTypeDef EXTIO_5;
-//	IOPinTypeDef EXTIO_6;
-//	IOPinTypeDef EXTIO_7;
-//
-//	IOPinTypeDef EEPROM_SCK;
-//	IOPinTypeDef EEPROM_SI;
-//	IOPinTypeDef EEPROM_SO;
-//	IOPinTypeDef EEPROM_NCS;
-//
-//	IOPinTypeDef MIXED0;
-//	IOPinTypeDef MIXED1;
-//	IOPinTypeDef MIXED2;
-//	IOPinTypeDef MIXED3;
-//	IOPinTypeDef MIXED4;
-//	IOPinTypeDef MIXED5;
-//	IOPinTypeDef MIXED6;
+	IOPinTypeDef EEPROM_SCK;
+	IOPinTypeDef EEPROM_SI;
+	IOPinTypeDef EEPROM_SO;
+	IOPinTypeDef EEPROM_NCS;
 
-#if defined(Landungsbruecke) || defined(LandungsbrueckeSmall) // HWID detection for Landungsbruecke v2.0+
+#if defined(Landungsbruecke)
+	IOPinTypeDef WIRELESS_TX;
+	IOPinTypeDef WIRELESS_RX;
+	IOPinTypeDef WIRELESS_NRST;
+	IOPinTypeDef MIXED0;
+	IOPinTypeDef MIXED1;
+	IOPinTypeDef MIXED2;
+	IOPinTypeDef MIXED3;
+	IOPinTypeDef MIXED4;
+	IOPinTypeDef MIXED5;
+	IOPinTypeDef MIXED6;
 	IOPinTypeDef ID_HW_0;
 	IOPinTypeDef ID_HW_1;
 	IOPinTypeDef ID_HW_2;
-#elif defined(LandungsbrueckeV3)
+#endif
+
+#if defined(LandungsbrueckeV3)
+	IOPinTypeDef ADC_VM;
+	IOPinTypeDef AIN0;
+	IOPinTypeDef AIN1;
+	IOPinTypeDef AIN2;
 	IOPinTypeDef WIFI_EN;
 	IOPinTypeDef WIFI_RST;
 #endif
 
-//	IOPinTypeDef DUMMY;
+	IOPinTypeDef DUMMY;
 } IOPinMapTypeDef;
 
 extern IOPinMapTypeDef IOMap;
