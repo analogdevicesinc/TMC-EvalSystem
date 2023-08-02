37b5536c61dffe50958b09250b73b659433d53ca
Leonard Kugis <leonard@kug.is>
2023-08-02 12:45:28 +0200

    TMC5241_eval: Removed individual register clearing bugfix.
    This was implemented for registers 0x31 and 0x66 due to a hardware bug.
    This is now fixed in pass 2 hardware.

--- boards/TMC5271_eval.c
+++ boards/TMC5271_eval.c
@@ -1165,17 +1165,6 @@ static uint32_t getMeasuredSpeed(uint8_t motor, int32_t *value)
 
 static void writeRegister(uint8_t motor, uint8_t address, int32_t value)
 {
-	//catch hardware bug
-	if (address == 0x31 && value == 2) {
-		tmc5271_writeInt(motorToIC(motor), address, 3);
-		return;
-	}
-	if (address == 0x66 && value == 2) {
-		tmc5271_writeInt(motorToIC(motor), address, 3);
-		return;
-	}
-
-
 	tmc5271_writeInt(motorToIC(motor), address, value);
 }
 
