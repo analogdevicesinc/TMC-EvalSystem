65f1cf7eafe2839a24b71dc230d9caf641f945c1
Lenard Heß <Lenard.Hess@analog.com>
2023-11-30 13:56:10 +0100

    Added a missing copyright header

--- hal/Landungsbruecke/makeFor.h
+++ hal/Landungsbruecke/makeFor.h
@@ -1,3 +1,9 @@
+/*******************************************************************************
+* Copyright © 2023 Analog Devices Inc. All Rights Reserved. This software is
+* proprietary & confidential to Analog Devices, Inc. and its licensors.
+*******************************************************************************/
+
+
 #ifndef Landungsbruecke
 	#define Landungsbruecke
 #endif
