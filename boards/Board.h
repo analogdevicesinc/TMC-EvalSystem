9e79d128734e67b0ead6286dc5798f0b1e59b0ad
ME <marvin.ellebrecht@analog.com>
2024-05-13 13:54:14 +0200

    Board.h: Removed TMC5130 header and tmc5130 from the motionControllerBoards struct

--- boards/Board.h
+++ boards/Board.h
@@ -36,7 +36,6 @@
 #include "tmc/ic/TMC5041/TMC5041.h"
 #include "tmc/ic/TMC5062/TMC5062.h"
 #include "tmc/ic/TMC5072/TMC5072.h"
-#include "tmc/ic/TMC5130/TMC5130.h"
 #include "tmc/ic/TMC8461/TMC8461.h"
 #include "tmc/ic/TMC8462/TMC8462.h"
 
@@ -161,7 +160,6 @@ typedef union {
     TMC5041TypeDef tmc5041;
     TMC5062TypeDef tmc5062;
     TMC5072TypeDef tmc5072;
-    TMC5130TypeDef tmc5130;
     TMC8461TypeDef tmc8461;
     TMC8462TypeDef tmc8462;
 } MotionControllerBoards;
