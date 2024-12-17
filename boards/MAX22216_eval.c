9cd9cdf19183885de1e4057023cdb149e81838df
Daniyal Hussain <Daniyal.Hussain@analog.com>
2024-12-17 15:42:52 +0100

    MAX22216: Fix Indentation.

--- boards/MAX22216_eval.c
+++ boards/MAX22216_eval.c
@@ -6,22 +6,24 @@
 * This software is proprietary to Analog Devices, Inc. and its licensors.
 *******************************************************************************/
 
-
 #include "boards/Board.h"
 #include "tmc/ic/MAX22216/MAX22216.h"
 #include "tmc/StepDir.h"
 #include "tmc/RAMDebug.h"
 #include "hal/Timer.h"
 
-#undef  MAX22216_MAX_VELOCITY
-#define MAX22216_MAX_VELOCITY  STEPDIR_MAX_VELOCITY
+#undef MAX22216_MAX_VELOCITY
+#define MAX22216_MAX_VELOCITY STEPDIR_MAX_VELOCITY
 
 // Stepdir precision: 2^17 -> 17 digits of precision
 #define STEPDIR_PRECISION (1 << 17)
 
-#define VM_MIN  51   // VM[V/10] min
-#define VM_MAX  360  // VM[V/10] max
+#define VM_MIN        51  // VM[V/10] min
+#define VM_MAX        360 // VM[V/10] max
+#define MOTORS        4
 #define DEFAULT_ICID  0
+#define TIMEOUT_VALUE 10 // 10 ms
+#define DEFAULT_MOTOR 0
 
 // Usage note: use 1 TypeDef per IC
 typedef struct
@@ -31,9 +33,34 @@ typedef struct
     uint8_t crc_en;
 } MAX22216TypeDef;
 static MAX22216TypeDef MAX22216;
-#define MOTORS 4
 
-#define TIMEOUT_VALUE 10 // 10 ms
+typedef struct
+{
+    IOPinTypeDef *CNTL[4];
+    IOPinTypeDef *CRC_EN;
+    IOPinTypeDef *FAULT_N;
+    IOPinTypeDef *STAT0;
+    IOPinTypeDef *STAT1;
+} PinsTypeDef;
+static PinsTypeDef Pins;
+
+typedef enum
+{
+    RAPID_FIRE_OFF = 0,
+    RAPID_FIRE_ON  = 1
+} rapid_fire_state_t;
+
+static SPIChannelTypeDef *MAX22216_SPIChannel;
+
+static bool rapid_fire_enabled[4]                  = {false, false, false, false};
+static uint32_t overflow_count_on[4]               = {0, 0, 0, 0};
+static uint32_t overflow_count_off[4]              = {0, 0, 0, 0};
+static uint32_t overflow_count[4]                  = {0, 0, 0, 0};
+static uint32_t rapid_fire_count_max[4]            = {0, 0, 0, 0};
+static uint32_t rapid_fire_count[4]                = {0, 0, 0, 0};
+static OTP_Status otp_status                       = OTP_STATUS_IDLE;
+static rapid_fire_state_t rapid_fire_state[4]      = {RAPID_FIRE_ON, RAPID_FIRE_ON, RAPID_FIRE_ON, RAPID_FIRE_ON};
+static rapid_fire_state_t rapid_fire_stop_state[4] = {RAPID_FIRE_OFF, RAPID_FIRE_OFF, RAPID_FIRE_OFF, RAPID_FIRE_OFF};
 
 static uint32_t right(uint8_t motor, int32_t velocity);
 static uint32_t left(uint8_t motor, int32_t velocity);
@@ -43,12 +70,10 @@ static uint32_t moveTo(uint8_t motor, int32_t position);
 static uint32_t moveBy(uint8_t motor, int32_t *ticks);
 static uint32_t GAP(uint8_t type, uint8_t motor, int32_t *value);
 static uint32_t SAP(uint8_t type, uint8_t motor, int32_t value);
-
 void MAX22216_init(void);
-static void checkErrors (uint32_t tick);
+static void checkErrors(uint32_t tick);
 static void deInit(void);
 static uint32_t userFunction(uint8_t type, uint8_t motor, int32_t *value);
-
 static void periodicJob(uint32_t tick);
 static void OTP_init(void);
 static void OTP_address(uint32_t address);
@@ -59,40 +84,6 @@ static void OTP_lock(void);
 static uint8_t reset(void);
 static void enableDriver(DriverState state);
 static void timer_overflow(timer_channel channel);
-
-static SPIChannelTypeDef *MAX22216_SPIChannel;
-static MAX22216TypeDef MAX22216;
-
-static bool rapid_fire_enabled[4] = { false, false, false, false };
-static uint32_t overflow_count_on[4] = { 0, 0, 0, 0 };
-static uint32_t overflow_count_off[4] = { 0, 0, 0, 0 };
-static uint32_t overflow_count[4] = { 0, 0, 0, 0 };
-static uint32_t rapid_fire_count_max[4] = { 0, 0, 0, 0 };
-static uint32_t rapid_fire_count[4] = { 0, 0, 0, 0 };
-
-static OTP_Status otp_status = OTP_STATUS_IDLE;
-
-
-typedef struct
-{
-    IOPinTypeDef *CNTL[4];
-    IOPinTypeDef *CRC_EN;
-    IOPinTypeDef *FAULT_N;
-    IOPinTypeDef *STAT0;
-    IOPinTypeDef *STAT1;
-} PinsTypeDef;
-
-typedef enum
-{
-    RAPID_FIRE_OFF = 0,
-    RAPID_FIRE_ON = 1
-} rapid_fire_state_t;
-
-static rapid_fire_state_t rapid_fire_state[4] = { RAPID_FIRE_ON, RAPID_FIRE_ON, RAPID_FIRE_ON, RAPID_FIRE_ON };
-static rapid_fire_state_t rapid_fire_stop_state[4] = { RAPID_FIRE_OFF, RAPID_FIRE_OFF, RAPID_FIRE_OFF, RAPID_FIRE_OFF };
-
-static PinsTypeDef Pins;
-
 static uint8_t restore(void);
 
 void max22216_readWriteSPI(uint16_t icID, uint8_t *data, size_t dataLength)
@@ -150,7 +141,7 @@ static uint32_t moveTo(uint8_t motor, int32_t position)
 
 static uint32_t moveBy(uint8_t motor, int32_t *ticks)
 {
-    if(motor >= MOTORS)
+    if (motor >= MOTORS)
         return TMC_ERROR_MOTOR;
 
     // determine actual position and add numbers of ticks to move
@@ -164,23 +155,30 @@ static uint32_t handleParameter(uint8_t readWrite, uint8_t motor, uint8_t type, 
     uint32_t errors = TMC_ERROR_NONE;
     //int32_t buffer = 0;
 
-    if(motor >= MOTORS)
+    if (motor >= MOTORS)
         return TMC_ERROR_MOTOR;
 
-    switch(type)
+    switch (type)
     {
     case 50: // Enable/disable rapid fire mode
-        if (readWrite == READ) {
+        if (readWrite == READ)
+        {
             *value = rapid_fire_enabled[motor] ? 1 : 0;
-        } else {
+        }
+        else
+        {
             rapid_fire_enabled[motor] = false;
-            if(*value) {
+            if (*value)
+            {
                 HAL.IOs->config->toOutput(Pins.CNTL[motor]);
-            } else {
-                overflow_count[motor] = 0;
+            }
+            else
+            {
+                overflow_count[motor]   = 0;
                 rapid_fire_state[motor] = RAPID_FIRE_ON;
                 rapid_fire_count[motor] = 0;
-                switch(rapid_fire_stop_state[motor]) {
+                switch (rapid_fire_stop_state[motor])
+                {
                 case RAPID_FIRE_ON:
                     HAL.IOs->config->setHigh(Pins.CNTL[motor]);
                     break;
@@ -191,8 +189,9 @@ static uint32_t handleParameter(uint8_t readWrite, uint8_t motor, uint8_t type, 
                 }
             }
             rapid_fire_enabled[motor] = *value;
-            for(size_t channel = 0; channel < ARRAY_SIZE(rapid_fire_enabled); channel++)
-                if(rapid_fire_enabled[channel]){
+            for (size_t channel = 0; channel < ARRAY_SIZE(rapid_fire_enabled); channel++)
+                if (rapid_fire_enabled[channel])
+                {
 #if defined(Landungsbruecke) || defined(LandungsbrueckeSmall)
                     Timer.setFrequency(TIMER_CHANNEL_1, 6000);
 #elif defined(LandungsbrueckeV3)
@@ -202,30 +201,42 @@ static uint32_t handleParameter(uint8_t readWrite, uint8_t motor, uint8_t type, 
         }
         break;
     case 51: // Rapid fire on-time
-        if(readWrite == READ) {
+        if (readWrite == READ)
+        {
             *value = overflow_count_on[motor];
-        } else if(readWrite == WRITE) {
+        }
+        else if (readWrite == WRITE)
+        {
             overflow_count_on[motor] = *value;
         }
         break;
     case 52: // Rapid fire off-time
-        if(readWrite == READ) {
+        if (readWrite == READ)
+        {
             *value = overflow_count_off[motor];
-        } else if(readWrite == WRITE) {
+        }
+        else if (readWrite == WRITE)
+        {
             overflow_count_off[motor] = *value;
         }
         break;
     case 53: // Rapid fire count
-        if(readWrite == READ) {
+        if (readWrite == READ)
+        {
             *value = rapid_fire_count_max[motor];
-        } else if(readWrite == WRITE) {
+        }
+        else if (readWrite == WRITE)
+        {
             rapid_fire_count_max[motor] = *value;
         }
         break;
     case 54: // Rapid fire stop state
-        if(readWrite == READ) {
+        if (readWrite == READ)
+        {
             *value = rapid_fire_stop_state[motor];
-        } else if(readWrite == WRITE) {
+        }
+        else if (readWrite == WRITE)
+        {
             rapid_fire_stop_state[motor] = *value;
         }
         break;
@@ -257,12 +268,16 @@ static void checkErrors(uint32_t tick)
 static uint32_t userFunction(uint8_t type, uint8_t motor, int32_t *value)
 {
     UNUSED(motor);
-    switch(type) {
+    switch (type)
+    {
     case 0:
-        if(*value) {
+        if (*value)
+        {
             HAL.IOs->config->setHigh(Pins.CRC_EN);
             MAX22216.crc_en = 1;
-        } else {
+        }
+        else
+        {
             HAL.IOs->config->setLow(Pins.CRC_EN);
             MAX22216.crc_en = 0;
         }
@@ -360,29 +375,34 @@ static void timer_overflow(timer_channel channel)
     debug_nextProcess();
 
     // Rapid fire
-    for(uint8_t motor = 0; motor < 4; motor++)
+    for (uint8_t motor = 0; motor < 4; motor++)
     {
-        if(rapid_fire_enabled[motor])
+        if (rapid_fire_enabled[motor])
         {
-            switch(rapid_fire_state[motor]) {
+            switch (rapid_fire_state[motor])
+            {
             case RAPID_FIRE_ON:
                 HAL.IOs->config->setHigh(Pins.CNTL[motor]);
                 overflow_count[motor]++;
-                if(overflow_count[motor] >= overflow_count_on[motor]) {
+                if (overflow_count[motor] >= overflow_count_on[motor])
+                {
                     rapid_fire_state[motor] = RAPID_FIRE_OFF;
-                    overflow_count[motor] = 0;
+                    overflow_count[motor]   = 0;
                 }
                 break;
             case RAPID_FIRE_OFF:
             default:
                 HAL.IOs->config->setLow(Pins.CNTL[motor]);
                 overflow_count[motor]++;
-                if(overflow_count[motor] >= overflow_count_off[motor]) {
+                if (overflow_count[motor] >= overflow_count_off[motor])
+                {
                     rapid_fire_count[motor]++;
-                    if((rapid_fire_count_max[motor] != 0) && (rapid_fire_count[motor] >= rapid_fire_count_max[motor])) {
+                    if ((rapid_fire_count_max[motor] != 0) && (rapid_fire_count[motor] >= rapid_fire_count_max[motor]))
+                    {
                         rapid_fire_enabled[motor] = false;
-                        rapid_fire_count[motor] = 0;
-                        switch(rapid_fire_stop_state[motor]) {
+                        rapid_fire_count[motor]   = 0;
+                        switch (rapid_fire_stop_state[motor])
+                        {
                         case RAPID_FIRE_ON:
                             HAL.IOs->config->setHigh(Pins.CNTL[motor]);
                             break;
@@ -393,7 +413,7 @@ static void timer_overflow(timer_channel channel)
                         }
                     }
                     rapid_fire_state[motor] = RAPID_FIRE_ON;
-                    overflow_count[motor] = 0;
+                    overflow_count[motor]   = 0;
                 }
                 break;
             }
@@ -407,12 +427,12 @@ void MAX22216_init(void)
     Pins.CNTL[1] = &HAL.IOs->pins->DIO9;
     Pins.CNTL[2] = &HAL.IOs->pins->DIO6;
     Pins.CNTL[3] = &HAL.IOs->pins->DIO7;
-    Pins.CRC_EN = &HAL.IOs->pins->DIO4;
+    Pins.CRC_EN  = &HAL.IOs->pins->DIO4;
     Pins.FAULT_N = &HAL.IOs->pins->DIO3;
-    Pins.STAT0 = &HAL.IOs->pins->DIO1;
-    Pins.STAT1 = &HAL.IOs->pins->DIO2;
+    Pins.STAT0   = &HAL.IOs->pins->DIO1;
+    Pins.STAT1   = &HAL.IOs->pins->DIO2;
 
-    MAX22216_SPIChannel = &HAL.SPI->ch2;
+    MAX22216_SPIChannel      = &HAL.SPI->ch2;
     MAX22216_SPIChannel->CSN = &HAL.IOs->pins->SPI2_CSN0;
 
     spi_setFrequency(MAX22216_SPIChannel, 12000000);
@@ -434,35 +454,35 @@ void MAX22216_init(void)
 
     HAL.IOs->config->setLow(Pins.CRC_EN);
 
-    Evalboards.ch2.config->reset        = reset;
-    Evalboards.ch2.config->restore      = restore;
-    Evalboards.ch2.config->state        = CONFIG_READY;
-    Evalboards.ch2.config->configIndex  = 0;
+    Evalboards.ch2.config->reset       = reset;
+    Evalboards.ch2.config->restore     = restore;
+    Evalboards.ch2.config->state       = CONFIG_READY;
+    Evalboards.ch2.config->configIndex = 0;
 
-    Evalboards.ch2.rotate               = rotate;
-    Evalboards.ch2.right                = right;
-    Evalboards.ch2.left                 = left;
-    Evalboards.ch2.stop                 = stop;
-    Evalboards.ch2.GAP                  = GAP;
-    Evalboards.ch2.SAP                  = SAP;
-    Evalboards.ch2.moveTo               = moveTo;
-    Evalboards.ch2.moveBy               = moveBy;
-    Evalboards.ch2.writeRegister        = max22216_writeRegister;
-    Evalboards.ch2.readRegister         = max22216_readRegister;
-    Evalboards.ch2.userFunction         = userFunction;
-    Evalboards.ch2.enableDriver         = enableDriver;
-    Evalboards.ch2.checkErrors          = checkErrors;
-    Evalboards.ch2.numberOfMotors       = MOTORS;
-    Evalboards.ch2.VMMin                = VM_MIN;
-    Evalboards.ch2.VMMax                = VM_MAX;
-    Evalboards.ch2.deInit               = deInit;
-    Evalboards.ch2.periodicJob          = periodicJob;
-    Evalboards.ch2.OTP_init             = OTP_init;
-    Evalboards.ch2.OTP_address          = OTP_address;
-    Evalboards.ch2.OTP_value            = OTP_value;
-    Evalboards.ch2.OTP_program          = OTP_program;
-    Evalboards.ch2.OTP_status           = OTP_status;
-    Evalboards.ch2.OTP_lock             = OTP_lock;
+    Evalboards.ch2.rotate         = rotate;
+    Evalboards.ch2.right          = right;
+    Evalboards.ch2.left           = left;
+    Evalboards.ch2.stop           = stop;
+    Evalboards.ch2.GAP            = GAP;
+    Evalboards.ch2.SAP            = SAP;
+    Evalboards.ch2.moveTo         = moveTo;
+    Evalboards.ch2.moveBy         = moveBy;
+    Evalboards.ch2.writeRegister  = writeRegister;
+    Evalboards.ch2.readRegister   = readRegister;
+    Evalboards.ch2.userFunction   = userFunction;
+    Evalboards.ch2.enableDriver   = enableDriver;
+    Evalboards.ch2.checkErrors    = checkErrors;
+    Evalboards.ch2.numberOfMotors = MOTORS;
+    Evalboards.ch2.VMMin          = VM_MIN;
+    Evalboards.ch2.VMMax          = VM_MAX;
+    Evalboards.ch2.deInit         = deInit;
+    Evalboards.ch2.periodicJob    = periodicJob;
+    Evalboards.ch2.OTP_init       = OTP_init;
+    Evalboards.ch2.OTP_address    = OTP_address;
+    Evalboards.ch2.OTP_value      = OTP_value;
+    Evalboards.ch2.OTP_program    = OTP_program;
+    Evalboards.ch2.OTP_status     = OTP_status;
+    Evalboards.ch2.OTP_lock       = OTP_lock;
 
     MAX22216.slaveAddress = 0;
     MAX22216.channel      = 0;
