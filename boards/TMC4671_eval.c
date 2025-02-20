bd926e2962290a1c238d66e60a0585160a204f25
Aysha Sundas <Aysha.Sundas@analog.com>
2025-02-20 17:54:36 +0100

    TMC4671-Eval: Fixed reading of AP 192

--- boards/TMC4671_eval.c
+++ boards/TMC4671_eval.c
@@ -642,8 +642,7 @@ static uint32_t handleParameter(uint8_t readWrite, uint8_t motor, uint8_t type, 
 	case 192:
 		// target velocity (PIDIN_TARGET_VELOCITY)
 		if(readWrite == READ) {
-			tmc4671_writeRegister(DEFAULT_ICID, TMC4671_INTERIM_ADDR, 2);
-			*value = tmc4671_readRegister(DEFAULT_ICID, TMC4671_INTERIM_DATA);
+            *value = rampGenerator[motor].targetVelocity;
 		}
 		else if(readWrite == WRITE)
 		{
