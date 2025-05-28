5566b4f8379aac86e41c3fe699b33b7bcbf018cc
Aysha Sundas <Aysha.Sundas@analog.com>
2025-05-28 12:34:48 +0200

    Main.c: Updated version string

--- main.c
+++ main.c
@@ -17,7 +17,7 @@
 #include "tmc/BoardAssignment.h"
 #include "tmc/RAMDebug.h"
 
-const char *VersionString = MODULE_ID "V310"; // module id and version of the firmware shown in the TMCL-IDE
+const char *VersionString = MODULE_ID "V311"; // module id and version of the firmware shown in the TMCL-IDE
 
 EvalboardsTypeDef Evalboards;
 
