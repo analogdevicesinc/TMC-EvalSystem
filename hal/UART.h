75d1e79cd54335c936e0ad4f93d36920fd305580
Lenard Heß <Lenard.Hess@analog.com>
2026-06-01 13:04:10 +0200

    UART.h: Whitespace cleanup

--- hal/UART.h
+++ hal/UART.h
@@ -13,22 +13,22 @@
 
 // Switchable UART pin configuration due to pinout changes in TMC2208 v1.2 -> TMC2208 v1.3 aswell as TMC2209
 typedef enum {
-	UART_PINS_1, // Default UART pinout (<= TMC2208 v1.2, UART_TXD = DIO17, UART_RXD = DIO18)
-	UART_PINS_2  // Alternate UART pinout (>= TMC2208 v1.3, UART_TXD = DIO10, UART_RXD = DIO11)
+    UART_PINS_1, // Default UART pinout (<= TMC2208 v1.2, UART_TXD = DIO17, UART_RXD = DIO18)
+    UART_PINS_2  // Alternate UART pinout (>= TMC2208 v1.3, UART_TXD = DIO10, UART_RXD = DIO11)
 } UART_Pins;
 
 typedef enum {
-	UART_MODE_DUAL_WIRE,
-	UART_MODE_SINGLE_WIRE,
-	UART_MODE_DUAL_WIRE_PushPull, // Use PushPull for TX instead of OpenDrain
+    UART_MODE_DUAL_WIRE,
+    UART_MODE_SINGLE_WIRE,
+    UART_MODE_DUAL_WIRE_PushPull, // Use PushPull for TX instead of OpenDrain
 } UART_Mode;
 
 typedef struct {
-	UART_Pins pinout;
-	UART_Mode mode;
-	uint16_t timeout;
-	RXTXTypeDef rxtx;
-	bool hideSingleWireEcho;
+    UART_Pins pinout;
+    UART_Mode mode;
+    uint16_t timeout;
+    RXTXTypeDef rxtx;
+    bool hideSingleWireEcho;
 } UART_Config;
 
 extern UART_Config UART;
